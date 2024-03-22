6ca9df1502115cd3a5c3df27f4fa4ce51b695777
Gustavo Kazuo <kazuo.hagy@gmail.com>
2024-03-21 22:11:40 -0300

    initial test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Post from './pages/Post';
+// import NotFound from './pages/NotFound';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita  gatinho mesmo <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div> dificil</div>
     </div>
   );
 }
