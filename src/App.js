7e88c7679308b739bb1e5680760fdc95f966432b
kazuo <kazucalado@gmail.com>
2024-03-24 01:36:00 -0300

    deus abencoe esse commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Post from './pages/Post';
+// import NotFound from './pages/NotFound';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div> dificil</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header style={{ display: "flex", justifyContent: "space-between" }}>
+      <h1>My Blog</h1>
+      <a href="/about">About</a>
+      <a href="">Home</a>
+      <a href="/contact">Contactsa</a>
+    </header>
+  );
+}
+export default Header;
