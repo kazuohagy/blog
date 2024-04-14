785497d6bc4eae8b7747323941ba835811abf993
kazuo <kazucalado@gmail.com>
2024-04-14 08:01:23 -0300

    eu quero viver

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React from 'react';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import Home from './pages/Home';
-// import About from './pages/About';
+// import About from './pages/About';s
 // import Post from './pages/Post';
 // import NotFound from './pages/NotFound';
 import Header from './components/Header';
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div> dificil</div>
+      <div> Este blog e deducado a passar um pouco dos meus estudos em programação.</div>
     </div>
   );
 }
